4860395dd6627c8c3e49e71aef97778429f955f8
Dickyharyanto1 <haryantodicky88@gmail.com>
2025-06-06 13:07:58 +0700

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -159,12 +159,12 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
   
   // Create array of login URLs to rotate through
   const loginUrls = [
-    "https://tinyurl.com/dxyvf6s9",
-    "https://tinyurl.com/dxyvf6s9",
-    "https://tinyurl.com/dxyvf6s9",
-    "https://tinyurl.com/dxyvf6s9",
-    "https://tinyurl.com/dxyvf6s9",
-    "https://tinyurl.com/dxyvf6s9"
+    "https://bit.ly/Backlinky",
+    "https://bit.ly/Backlinky",
+    "https://bit.ly/Backlinky",
+    "https://bit.ly/Backlinky",
+    "https://bit.ly/Backlinky",
+    "https://bit.ly/Backlinky"
   ];
   
   // Convert the array to JSON string for AMP state
@@ -583,13 +583,13 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
   <!-- Header -->
   <header class="header">
     <div class="logo-container">
-      <a href="https://tinyurl.com/dxyvf6s9">
+      <a href="https://bit.ly/Backlinky">
         <amp-img class="logo" src="https://pub-bc2ee8893baf416c8c23af0718d51fc3.r2.dev/slotgacorwin.gif" width="200" height="50" layout="fixed" alt="${siteName.toUpperCase()}"></amp-img>
       </a>
     </div>
     <nav class="main-nav">
-      <a href="https://tinyurl.com/dxyvf6s9" class="nav-link">Home</a>
-      <a href="https://tinyurl.com/dxyvf6s9" 
+      <a href="https://bit.ly/Backlinky" class="nav-link">Home</a>
+      <a href="https://bit.ly/Backlinky" 
          class="login-btn login-btn-animated"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
@@ -621,7 +621,7 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
     
     <div class="site-slogan">Situs Slot Gacor Paling Aman Ga Pake Ribet!</div>
     <div class="action-buttons">
-      <a href="https://tinyurl.com/dxyvf6s9" 
+      <a href="https://bit.ly/Backlinky" 
          class="register-btn"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
@@ -629,7 +629,7 @@ function generateAmpHtml(siteName, canonicalUrl, allSites) {
              currentUrlIndex: (siteData.currentUrlIndex + 1) % siteData.loginUrls.length
            }
          })">Daftar ${siteName}</a>
-      <a href="https://tinyurl.com/dxyvf6s9" 
+      <a href="https://bit.ly/Backlinky" 
          class="login-block-btn"
          [href]="siteData.loginUrls[siteData.currentUrlIndex]"
          on="tap:AMP.setState({
